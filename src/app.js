de730bed7f2587cadf96aaa3e3eb96141e9fbc58
matheusc3m <matheusfernandes3m@hotmail.com>
2020-11-22 05:29:04 -0300

    update2

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import routes from'./routes';
+import routes from './routes';
 
-class App{
-  constructor(){
-    this.server=express();
-   //  conexão com o banco id:root senha:admin
-   //  https://cloud.mongodb.com/v2/5fb45389e84ccc2788e36fe7#security/database/users
-    mongoose.connect('mongodb+srv://root:admin@e-lab.vjutz.mongodb.net/<E-lab>?retryWrites=true&w=majority',{
+class App {
+  constructor() {
+    this.server = express();
+    //  conexão com o banco id:root senha:admin
+    //  https://cloud.mongodb.com/v2/5fb45389e84ccc2788e36fe7#security/database/users
+    mongoose.connect('mongodb+srv://root:admin@e-lab.vjutz.mongodb.net/<E-lab>?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -15,12 +15,12 @@ class App{
     this.middlewares();
     this.routes();
   }
-  middlewares(){
-      this.server.use(express.json());
+  middlewares() {
+    this.server.use(express.json());
   }
-  routes(){
-      this.server.use(routes)
+  routes() {
+    this.server.use(routes)
   }
 
 }
-module.exports=new App().server;
+module.exports = new App().server;
